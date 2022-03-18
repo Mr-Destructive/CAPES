92d6bd9096e3b61721d2f47e39867ba4a6a7ae7c
Mr-Destructive <gormeet711@gmail.com>
2022-03-18 12:33:01 +0530

    fix settings

--- core/local_settings.py
+++ core/local_settings.py
@@ -1,2 +1,13 @@
-ALLOWED_HOSTS = ['192.168.99.100',]
+ALLOWED_HOSTS = ['192.168.99.100','127.0.0.1',]
 SECRET_KEY="fd389fcc60bdf0670c2bf2ce9be721f8b57bdf2d52cc47c8"
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'capes',
+        'USER': 'postgres',
+        'PASSWORD': '@1234567',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
